dbbd4686a01ba6bc2e9840356d2f0dadbca1bf98
uma <umathan13@gmail.com>
2018-11-08 19:04:04 -0500

    adding files

---
+++ Controllers/ComicBookController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace comic_book_gallery.Controllers
+{
+    public class ComicBookController : Controller
+    {
+        public ActionResult Detail()
+        {
+            /* if(DateTime.Today.DayOfWeek == DayOfWeek.Tuesday)
+             {
+                 return new RedirectResult("/");
+             }
+
+             return new ContentResult()
+             {
+               Content =  "Hello from the comic books controller!"
+             };
+             */
+
+            ViewBag.SeriesTitle = "The Amazing Spider-Man";
+            ViewBag.IssueNumber = 700;
+            ViewBag.Description = "<p>Final issue! Witness the final hours of Doctor Octopus’ life and his one, last, great act of revenge! Even if Spider-Man survives... <strong>will Peter Parker?</strong></ p >";
+            ViewBag.Artists = new string[]
+            {
+                "Script:  Dan Slott",
+                "Pencils: Humberto Ramos",
+                "Inks: Victor Olazaba",
+                "Colors: Edgar Delgado",
+                "Letters: Chris Eliopoulos"
+            };
+
+
+            return View();
+        }
+    }
+}
